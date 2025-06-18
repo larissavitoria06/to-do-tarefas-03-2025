31bd6a79eaaabe8750207776c60691d6265e90a6
larissavitoria06 <larissavitoria.ssntos12@gmail.com>
2025-06-18 10:48:07 -0300

    dgfajfasdf

---
+++ api/prisma/migrations/20250618134712_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE `Usuario` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Tarefa` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `descricao` VARCHAR(100) NOT NULL,
+    `setor` VARCHAR(50) NOT NULL,
+    `prioridade` VARCHAR(191) NOT NULL,
+    `status` ENUM('AFAZER', 'FAZENDO', 'PRONTO') NOT NULL,
+    `usuarioId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Tarefa` ADD CONSTRAINT `Tarefa_usuarioId_fkey` FOREIGN KEY (`usuarioId`) REFERENCES `Usuario`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
